e5d6520830d926b4d591e024c41d9eafd5fd2bc9
AlexandreGuimaraes <alexandre.webapps@gmail.com>
2016-04-08 04:21:06 -0400

    Added photos and videos
    Added photo e videos pages, new js code e files. Css modification.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,21 @@
 class PagesController < ApplicationController
   def home
-    
+
   end
-  
+
   def about
-    
+
+  end
+
+  def photos
+
+  end
+
+  def photos01
+
+  end
+
+  def videos
+
   end
 end
